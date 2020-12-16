a9ddcc3250e72fcafc662a2044f31810508522cd
Eliot Sanford <eliot587@gmail.com>
2020-12-16 12:46:31 -0600

    Build a redux application

---
+++ index.js
@@ -0,0 +1,40 @@
+const reducer = Redux.combineReducers({
+    todos: (state = [], action) => {
+      const newState = Object.assign([], state);
+      if (action.type == 'add') {
+        newState.push(action.item);
+      }
+      if (action.type == 'remove') {
+        newState.splice(action.index, 1);
+      }
+      return newState;
+    }
+  });
+  
+  const store = Redux.createStore(reducer);
+  
+  const render = () => {
+    const container = document.getElementById('container');
+    container.innerHTML = '';
+    const state = store.getState();
+    state.todos.forEach((todo, i) => {
+      const e = document.createElement('div');
+      e.innerHTML = todo;
+      container.appendChild(e);
+      e.onclick = () => {
+        store.dispatch({
+          type: 'remove',
+          index: i
+        });
+        render();
+      }
+    });
+  };
+  
+  document.getElementById('submit-todo').onclick = () => {
+    store.dispatch({
+      type: 'add',
+      item: document.getElementById('todo').value
+    });
+    render();
+  };
